389002118f9677fd9a3c17e717802254b4f95cbc
k0nsl <i.am@k0nsl.org>
2017-05-05 03:44:23 +0200

    Update vnstati.sh
    testing, testing.

--- vnstati.sh
+++ vnstati.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # "just in case"
-rm /var/lib/transmission-daemon/downloads/stats/vnstat-summary.png
-rm /var/lib/transmission-daemon/downloads/stats/vnstat-daily.png
-rm /var/lib/transmission-daemon/downloads/stats/vnstat-monthly.png
-rm /var/lib/transmission-daemon/downloads/stats/vnstat-hourly.png
-rm /var/lib/transmission-daemon/downloads/stats/vnstat-top10.png
-# generate the graphs; summary, dailym, monthly, hourly and top10
+rm -rf /var/lib/transmission-daemon/downloads/stats/vnstat-summary.png
+rm -rf /var/lib/transmission-daemon/downloads/stats/vnstat-daily.png
+rm -rf /var/lib/transmission-daemon/downloads/stats/vnstat-monthly.png
+rm -rf /var/lib/transmission-daemon/downloads/stats/vnstat-hourly.png
+rm -rf /var/lib/transmission-daemon/downloads/stats/vnstat-top10.png
+# generate the graphs; summary, daily, monthly, hourly and top10
 vnstati -vs --config /etc/vnstat.conf -o /var/lib/transmission-daemon/downloads/stats/vnstat-summary.png
 vnstati -d --config /etc/vnstat.conf -o /var/lib/transmission-daemon/downloads/stats/vnstat-daily.png
 vnstati -m --config /etc/vnstat.conf -o /var/lib/transmission-daemon/downloads/stats/vnstat-monthly.png
